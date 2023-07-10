facadd20cb3fe4d3cfc696d60fad5e29cdde5d6c
navneet nishchal <navneetnishchal1420@gmail.com>
2023-07-10 06:02:29 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,86 @@
-import Image from 'next/image'
+'use client'
+import { useState } from 'react';
+import Image from 'next/image';
 import styles from './page.module.css'
+import image1 from '../public/image1.jpg'
+import image2 from '../public/image2.jpg'
+import image3 from '../public/image3.jpg'
+import image4 from '../public/image4.jpg'
+import image5 from '../public/image5.jpg'
+import image6 from '../public/image6.jpg'
+import ideal from '../public/ideal.jpg'
+
 
 export default function Home() {
+
+  const [iimage,setIimage]=useState(ideal);
+  const [output,setOutput]=useState("We Help you in checking vehicle health ")
+   
+  const uploadImage = async (e) => {
+    const files = e.target.files;
+    console.log("some error occured");
+    console.log(files);
+    console.log("this is the name of the file");
+    console.log(files[0].name);
+    if(files[0].name==='image1.jpg'){
+      setIimage(image1);
+      setOutput("This vehicle is not in working condition");
+    }
+    if(files[0].name==='image2.jpg'){
+      setIimage(image2);
+      setOutput("This vehicle is not in working condition");
+    }
+    if(files[0].name==='image3.jpg'){
+      setIimage(image3);
+      setOutput("This vehicle is not in working condition");
+    }
+    if(files[0].name==='image4.jpg'){
+      setIimage(image4);
+      setOutput("This vehicle is not in working condition");
+    }
+    if(files[0].name==='image5.jpg'){
+      setIimage(image5);
+      setOutput("This vehicle is in working condition");
+    }
+    if(files[0].name==='image6.jpg'){
+      setIimage(image6);
+      setOutput("This vehicle is in working condition");
+    }
+    
+   }
+
+
+
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+        <div className={styles.maindiv}>
+          <div className={styles.mainfirst}>
+            <div className={styles.inner1}>
+            <input
+                className={styles.formclass}
+                type="file"
+                id='file1'
+                onChange={uploadImage}
+               
+              />
+              <label For='file1'>Select An Image To check</label>
+
+            </div>
+            <div className={styles.inner2}>
+              <Image src={iimage}  alt='loading'   width={0}
+                          height={0}
+                          sizes="100vw"
+                          style={{ width: '100%', height: '100%' }}/>
+               
+            </div>
+
+          </div>
+          <div className={styles.mainsec}>
+              <div className={styles.output}>{output}</div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
